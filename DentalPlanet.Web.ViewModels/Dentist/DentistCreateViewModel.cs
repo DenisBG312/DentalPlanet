46f0bbc38b6eda1a8f5b3a9e789bf27c844e5436
Denis Nedkov <113765690+DenisBG312@users.noreply.github.com>
2024-12-24 01:17:46 +0200

    fixed some logic

--- DentalPlanet.Web.ViewModels/Dentist/DentistCreateViewModel.cs
+++ DentalPlanet.Web.ViewModels/Dentist/DentistCreateViewModel.cs
@@ -1,26 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 
-namespace DentalPlanet.Data.Models
+namespace DentalPlanet.Web.ViewModels.Dentist
 {
-    public class Dentist
+    public class DentistCreateViewModel
     {
-        [Key]
-        public string Id { get; set; } = Guid.NewGuid().ToString();
         [Required]
         public string Specialty { get; set; }
 
         [MaxLength(15)]
         public string Availability { get; set; }
+
         [Required]
+        [Display(Name = "User")] 
         public string UserId { get; set; }
-        [ForeignKey(nameof(UserId))]
-        public ApplicationUser User { get; set; }  
+
     }
 }
--- DentalPlanet.Web/Controllers/DentistController.cs
+++ DentalPlanet.Web/Controllers/DentistController.cs
@@ -1,12 +1,15 @@
 ﻿using DentalPlanet.Data;
+using DentalPlanet.Data.Models;
+using DentalPlanet.Web.ViewModels.Dentist;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace DentalPlanet.Web.Controllers
 {
     public class DentistController : Controller
     {
-        private ApplicationDbContext context;
+        private readonly ApplicationDbContext context;
         public DentistController(ApplicationDbContext context)
         {
             this.context = context;
@@ -19,6 +22,62 @@ namespace DentalPlanet.Web.Controllers
                 .ToListAsync();
             
             return View(dentists);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var availableUsers = await context.Users
+                .Where(user => !context.Dentists.Any(dentist => dentist.UserId == user.Id))
+                .ToListAsync();
+
+            if (!availableUsers.Any())
+            {
+                TempData["NoUsers"] = "There are no users left to assign as dentists.";
+            }
+
+
+            ViewData["Users"] = new SelectList(availableUsers, "Id", "UserName");
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(DentistCreateViewModel model)
+        {
+            ModelState.Remove("User");
+            if (ModelState.IsValid)
+            {
+                bool userAlreadyDentist = await context.Dentists.AnyAsync(d => d.UserId == model.UserId);
+                if (userAlreadyDentist)
+                {
+                    ModelState.AddModelError("UserId", "The selected user is already assigned as a dentist.");
+                }
+                else
+                {
+                    var dentist = new Dentist
+                    {
+                        Specialty = model.Specialty,
+                        Availability = model.Availability,
+                        UserId = model.UserId
+                    };
+
+                    context.Add(dentist);
+                    await context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+
+                var availableUsers = await context.Users
+                    .Where(user => !context.Dentists.Any(dentist => dentist.UserId == user.Id))
+                    .ToListAsync();
+                
+                ViewData["Users"] = new SelectList(availableUsers, "Id", "UserName");
+                return View(model);
+            }
+
+            var users = await context.Users.ToListAsync();
+            ViewData["Users"] = new SelectList(users, "Id", "UserName");
+
+            return View(model);
         }
 
         public async Task<IActionResult> Details(string id)
